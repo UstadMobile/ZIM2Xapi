15f542ac3853b93875de25ff41b09c858fa6bd27
Samih Mustafa <samihmustafa@gmail.com>
2024-10-30 20:37:52 +0400

    throw exceptions if required js files missing

--- app/src/main/kotlin/com/ustadmobile/zim2xapi/AddxAPIStatementUseCase.kt
+++ app/src/main/kotlin/com/ustadmobile/zim2xapi/AddxAPIStatementUseCase.kt
@@ -30,11 +30,11 @@ class AddxAPIStatementUseCase {
 
         val passingGradeDeclaration = "const passingGrade = $passingGrade;\n"
         val scoreFile = File(zimFolder, "/assets/perseus/score-tracker.js")
-        val scoreText = this::class.java.classLoader.getResource("score-tracker.js")?.readText() ?: ""
+        val scoreText = this::class.java.classLoader.getResource("score-tracker.js")?.readText() ?: throw Exception("score-tracker.js not found")
         scoreFile.writeText(passingGradeDeclaration + scoreText)
 
         val widgetsFile = File(zimFolder, "/assets/perseus/khan-widgets.js")
-        val widgetsText = this::class.java.classLoader.getResource("khan-widgets.js")?.readText() ?: ""
+        val widgetsText = this::class.java.classLoader.getResource("khan-widgets.js")?.readText() ?: throw Exception("khan-widgets.js not found")
         widgetsFile.writeText(widgetsText)
 
         val originalJS = perseusFile.readText()
