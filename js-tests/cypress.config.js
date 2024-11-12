f07bf2942806b7957058be7b353be0515036b269
Samih Mustafa <samihmustafa@gmail.com>
2024-11-13 00:12:42 +0400

    add video and mocha reporter for CI, update zims

--- js-tests/cypress.config.js
+++ js-tests/cypress.config.js
@@ -3,10 +3,21 @@ const fs = require('fs-extra');
 const path = require('path');
 
 module.exports = defineConfig({
+  video: true,
   e2e: {
     specPattern: 'e2e/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'e2e/cypress/support/e2e.js',
     baseUrl: 'http://localhost:8081',
+    chromeWebSecurity: false,
+    reporter: 'mocha-junit-reporter', //refer here for report options: https://www.npmjs.com/package/mocha-junit-reporter#full-configuration-options
+            reporterOptions: {
+                mochaFile: 'e2e/cypress/results/my-test-output-.[hash].xml',
+                testsuitesTitle: true,
+                suiteFilename: true,
+                suiteTitleSeparatedBy: '.',   // suites separator, default is space (' '), or period ('.') in jenkins mode
+                jenkinsMode: true,
+                toConsole: false
+            },
    
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -31,6 +42,6 @@ module.exports = defineConfig({
 
     },
     screenshotsFolder: 'e2e/cypress/screenshots', // Custom path for screenshots
-    videosFolder: 'e2e/cypress/videos',           // Custom path for videos
+    videosFolder: 'e2e/cypress/videos',  // Custom path for videos
   },
 });
